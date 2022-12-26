9a6d5b212de99d37536c7aa941c06bc9e3ff9714
katerina-leo <81039066+katerina-leo@users.noreply.github.com>
2022-12-26 11:49:39 +0300

    paddings

--- source/js/index.js
+++ source/js/index.js
@@ -1,9 +1,10 @@
-const navMain = document.querySelector('.main-nav');
+const toggleMenu = () => {
+  const navMain = document.querySelector('.main-nav');
   const navToggle = document.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--no-js');
 
-  navToggle.addEventListener('click', function() {
+  navToggle.addEventListener('click', () => {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
@@ -12,3 +13,12 @@ const navMain = document.querySelector('.main-nav');
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+}
+
+
+const init = () => {
+  toggleMenu()
+}
+
+init();
---
+++ source/js/toggleMenu.js
@@ -0,0 +1,17 @@
+export const toggleMenu = () => {
+  const navMain = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--no-js');
+
+  navToggle.addEventListener('click', () => {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+}
